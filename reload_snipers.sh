ebb3cf231b223c82acc98147d8a4bf4b4b88f1e9
João Costa <joaocosta@zonalivre.org>
2016-04-17 00:45:54 +0000

    Bash script to load multiple fx sniper scripts

---
+++ reload_snipers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -euo pipefail
+
+. ~/fxcm.real
+
+cd /root/Finance-snipers
+git pull origin master
+
+function load_sniper {
+    SNIPER_ID=sniper-${SYMBOL}-${DIRECTION}
+    SNIPER_CONTAINER_ID=$(docker ps -a -q -f name=${SNIPER_ID})
+    if [ "$SNIPER_CONTAINER_ID" != "" ]; then
+        docker rm -f ${SNIPER_ID}
+    fi
+    docker run \
+            --restart=always \
+            --name ${SNIPER_ID} \
+            --link fxdata \
+            -h ${SNIPER_ID}.fxhistoricaldata.com \
+            -v /root/fxtrader.cfg:/etc/fxtrader \
+            -v /root/Finance-snipers/bin:/root/snipers \
+            -e FXCM_USERNAME -e FXCM_PASSWORD -e FXCM_ACCOUNT_TYPE \
+            -e SYMBOL -e FXCM_SYMBOL \
+            -e MAX_EXPOSURE -e EXPOSURE_INCREMENT -e DIRECTION \
+            --log-driver=journald \
+            -d fxtrader/scripts sh -c 'exec /root/snipers/fx-sniper.pl'
+}
+
+SYMBOL=AUDUSD FXCM_SYMBOL='AUD/USD' \
+MAX_EXPOSURE=120000 EXPOSURE_INCREMENT=10000 DIRECTION=long \
+load_sniper
+
+SYMBOL=XAGUSD FXCM_SYMBOL='XAG/USD' \
+MAX_EXPOSURE=300 EXPOSURE_INCREMENT=50 DIRECTION=long \
+load_sniper
+
+
+#FXCM_USERNAME=GBD118836001 FXCM_PASSWORD='5358' FXCM_ACCOUNT_TYPE=Demo \
