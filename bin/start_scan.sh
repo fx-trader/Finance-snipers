b37f4d579015b2ea5418720fa3ec1aebf5376a58
João Costa <joaocosta@zonalivre.org>
2016-10-16 14:56:45 -0400

    tweaks

--- bin/start_scan.sh
+++ bin/start_scan.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-docker run --restart=always --name signal-scan -h signal-scan.fxhistoricaldata.com -v /root/src:/root/src --log-driver=journald -d fxtrader/finance-hostedtrader sh -c 'exec /usr/bin/perl /root/src/signal_check.pl'
+SNIPER_DIR=`pwd -P`
+
+docker run  \
+    --restart=always \
+    --name signal-scan \
+    -h signal-scan.fxhistoricaldata.com \
+    -v $SNIPER_DIR/bin:/root/snipers \
+    --log-driver=journald \
+    -d fxtrader/finance-hostedtrader \
+    sh -c 'exec /usr/bin/perl /root/snipers/signal_check.pl'
