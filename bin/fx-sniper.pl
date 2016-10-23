ee761e2d712a13c7ab2efa7596e6a87f9ad62236
João Costa <joaocosta@zonalivre.org>
2016-10-23 00:03:00 +0000

    Tweaks
    new data structure returned by finance::hostedtrader
    adjust loglevel from debug to info

--- bin/fx-sniper.pl
+++ bin/fx-sniper.pl
@@ -16,8 +16,8 @@ my $time_limit = time() + 1320; #Force a restart after 1320 seconds, to cleanup 
 
 # Initialize Logger
 my $log_conf = q(
-log4perl rootLogger = DEBUG, SCREEN
-#log4perl rootLogger = DEBUG, LOG1, SCREEN
+log4perl rootLogger = INFO, SCREEN
+#log4perl rootLogger = INFO, LOG1, SCREEN
 log4perl.appender.SCREEN         = Log::Log4perl::Appender::Screen
 log4perl.appender.SCREEN.stderr  = 0
 log4perl.appender.SCREEN.layout  = Log::Log4perl::Layout::PatternLayout
@@ -43,29 +43,29 @@ my $check_interval = 30;        # How many seconds to wait for before checking a
 my $direction = $ENV{DIRECTION} || $logger->logdie("DIRECTION NOT DEFINED");
 $logger->logdie("DIRECTION has to be either 'long' or 'short'") unless ($direction eq 'long' or $direction eq 'short');
 
-$logger->debug("Sniper reporting for duty");
-$logger->debug("ACCOUNT ID = $ENV{FXCM_USERNAME} ($ENV{FXCM_ACCOUNT_TYPE})");
-$logger->debug("SYMBOL = $symbol");
-$logger->debug("INTERVAL = $check_interval seconds");
+$logger->info("Sniper reporting for duty");
+$logger->info("ACCOUNT ID = $ENV{FXCM_USERNAME} ($ENV{FXCM_ACCOUNT_TYPE})");
+$logger->info("SYMBOL = $symbol");
+$logger->info("INTERVAL = $check_interval seconds");
 
 while (1) {
     my $fxcm = Finance::FXCM::Simple->new($ENV{FXCM_USERNAME}, $ENV{FXCM_PASSWORD}, $ENV{FXCM_ACCOUNT_TYPE}, 'http://www.fxcorporate.com/Hosts.jsp');
     #last if ( -f "/tmp/snipers_disengage" );
     if (time() > $time_limit) {
-        $logger->debug("Exiting to allow memory cleanup");
+        $logger->info("Exiting to allow memory cleanup");
         last;
     }
 
     sleep($check_interval);
 
-    $logger->debug("--------------------");
+    $logger->info("--------------------");
 
     my $bid = $fxcm->getBid($fxcm_symbol); # The price I can sell at
     my $ask = $fxcm->getAsk($fxcm_symbol); # The price I can buy at
-    $logger->debug("BID = $bid");
-    $logger->debug("ASK = $ask");
+    $logger->info("BID = $bid");
+    $logger->info("ASK = $ask");
     my $spread = sprintf("%.5f", $ask - $bid);
-    $logger->debug("SPREAD = $spread");
+    $logger->info("SPREAD = $spread");
 
     # macd2_data not used at the moment, just call it here for the side effect of
     # macd value over 2hour timeframe being printed in the logs
@@ -76,22 +76,22 @@ while (1) {
 
     my $symbol_trades = $fxcm->getTradesForSymbol($fxcm_symbol);
     my $symbol_exposure = sum0 map { $_->{size} }  @$symbol_trades;
-    $logger->debug("$symbol exposure = $symbol_exposure");
+    $logger->info("$symbol exposure = $symbol_exposure");
 
     my @trades = sort { $b->{openDate} cmp $a->{openDate} } grep { $_->{direction} eq $direction } @{ $symbol_trades || [] };
 
-    $logger->debug("Max Exposure = $max_exposure");
-    $logger->debug("Increment = $exposure_increment");
+    $logger->info("Max Exposure = $max_exposure");
+    $logger->info("Increment = $exposure_increment");
     my $min_trade_size = $fxcm->getBaseUnitSize($fxcm_symbol);
-    $logger->debug("Min trade size = $min_trade_size");
+    $logger->info("Min trade size = $min_trade_size");
 
     if ($trades[0]) {
         my $most_recent_trade = $trades[0];
         my $seconds_ago = time() - convertToEpoch($most_recent_trade->{openDate});
-        $logger->debug("LAST TRADE [$most_recent_trade->{openDate}, ${seconds_ago}s ago] = " . $most_recent_trade->{openPrice});
+        $logger->info("LAST TRADE [$most_recent_trade->{openDate}, ${seconds_ago}s ago] = " . $most_recent_trade->{openPrice});
         my $latest_price = $fxcm->getAsk($fxcm_symbol);
-        $logger->debug("Latest price = $latest_price");
-        $logger->debug("Skip trade opened recently") and next if ($seconds_ago < 3600);
+        $logger->info("Latest price = $latest_price");
+        $logger->info("Skip trade opened recently") and next if ($seconds_ago < 3600);
     }
 
     my $multiplier;
@@ -100,35 +100,35 @@ while (1) {
         my $pivot_data = getIndicatorValue($symbol, '4hour', 'max(close,14)');
         my $rsi_trigger = 35;
         $multiplier = ($pivot_data->[1] - $ask ) / $atr_data->[1];
-        $logger->debug("Set RSI trigger at $rsi_trigger");
-        $logger->debug("Skip rsi") and next if ($rsi_data->[1] >= $rsi_trigger);
-        $logger->debug("Skip macd") and next if ($macd4_data->[1] >= 0);
+        $logger->info("Set RSI trigger at $rsi_trigger");
+        $logger->info("Skip rsi") and next if ($rsi_data->[1] >= $rsi_trigger);
+        $logger->info("Skip macd") and next if ($macd4_data->[1] >= 0);
     } else {
         my $pivot_data = getIndicatorValue($symbol, '4hour', 'min(close,14)');
         my $rsi_trigger = 65;
         $multiplier = ($bid - $pivot_data->[1]) / $atr_data->[1];
-        $logger->debug("Set RSI trigger at $rsi_trigger");
-        $logger->debug("Skip rsi") and next if ($rsi_data->[1] <= $rsi_trigger);
-        $logger->debug("Skip macd") and next if ($macd4_data->[1] <= 0);
+        $logger->info("Set RSI trigger at $rsi_trigger");
+        $logger->info("Skip rsi") and next if ($rsi_data->[1] <= $rsi_trigger);
+        $logger->info("Skip macd") and next if ($macd4_data->[1] <= 0);
     }
 
-    $logger->debug("Multiplier = $multiplier");
+    $logger->info("Multiplier = $multiplier");
     my $adjusted_exposure_increment = int($exposure_increment * abs($multiplier) / $min_trade_size) * $min_trade_size;
-    $logger->debug("Adjusted incremental position size = $adjusted_exposure_increment");
+    $logger->info("Adjusted incremental position size = $adjusted_exposure_increment");
 
-    $logger->debug("Skip multiplier <1") and next if ( $multiplier < 1);
+    $logger->info("Skip multiplier <1") and next if ( $multiplier < 1);
 
-    $logger->debug("Skip exposure") and next if ( $max_exposure < $symbol_exposure );
+    $logger->info("Skip exposure") and next if ( $max_exposure < $symbol_exposure );
 
     my $open_position_size = ($symbol_exposure + $adjusted_exposure_increment > $max_exposure ? $max_exposure - $symbol_exposure : $adjusted_exposure_increment );
-    $logger->debug("Add position to $symbol ($open_position_size)");
+    $logger->info("Add position to $symbol ($open_position_size)");
     $fxcm->openMarket($fxcm_symbol, HOSTED_TRADER_2_FXCM_DIRECTION($direction), $open_position_size);
 
     zap( { subject => "fx-sniper: openmarket", message => "$fxcm_symbol\n$direction\nMULTIPLIER = $multiplier\nPOSITION SIZE = $open_position_size\nASK Price = $ask\nRSI ($rsi_data->[0]) = $rsi_data->[1]\n" } );
     $fxcm = undef; #This logouts from the FXCM session, and can take a few seconds to return
 }
 
-$logger->debug("Sniper disengaged");
+$logger->info("Sniper disengaged");
 
 #unlink("/tmp/snipers_disengage");
 exit(1);
@@ -158,11 +158,12 @@ sub getIndicatorValue {
             'maxLoadedItems'  => 50000,
             'numItems' => 1,
         });
-    $signal_processor = undef;
-    $logger->logdie("Failed to retrieve indicator '$indicator'") if (!$data || !$data->[0]);
-    $logger->debug("$indicator [$data->[0]->[0]] ($tf) = $data->[0]->[1]");
 
-    return $data->[0];
+    $signal_processor = undef;
+    $logger->logdie("Failed to retrieve indicator '$indicator'") if (!$data || !$data->{data} || !$data->{data}->[0]);
+    $logger->info("$indicator [$data->{data}->[0]->[0]] ($tf) = $data->{data}->[0]->[1]");
+
+    return $data->{data}->[0];
 }
 
 sub convertToEpoch {
@@ -189,7 +190,7 @@ sub zap {
 
     if ($response->is_success) {
         my $response_body = $response->as_string();
-        $logger->debug($response_body);
+        $logger->info($response_body);
         my $result = decode_json($response->content);
         if ($result->{status} && $result->{status} eq 'success') {
             $logger->info("Sent request to $url successfully");
