1b86d9acd4172e8214933d2ff30c503c007670bd
João Costa <joaocosta@zonalivre.org>
2018-12-21 21:11:03 +0000

    Helper scripts

---
+++ bin/findtrades.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+
+
+my $emafilter = Filter::EMARatio->new();
+my $rsifilter = Filter::RSI->new();
+
+my %tradeable;
+
+my $results = $emafilter->filter();
+
+my (%longs, %shorts);
+
+foreach my $result (@{ $results->{long} }) {
+    $longs{$result->[0]}++;
+}
+foreach my $result (@{ $results->{short} }) {
+    $shorts{$result->[0]}++;
+}
+
+
+$results = $rsifilter->filter();
+
+foreach my $result (@{ $results->{long} }) {
+    $longs{$result->[0]}++;
+}
+foreach my $result (@{ $results->{short} }) {
+    $shorts{$result->[0]}++;
+}
+
+
+print "Long : ", join(", ", keys %longs), "\n";
+print "Short: ", join(", ", keys %shorts), "\n";
+
+
+package Filter::EMARatio;
+
+use Moo;
+
+sub filter {
+    my $self = shift;
+
+    my $fxapi = FXAPI->new();
+    my $data = $fxapi->screen( expression => "close/ema(close,200)" );
+
+    my %results;
+
+    my @sorted = sort {
+        my $v1 = $a->[2] > 1 ? $a->[2] - 1 : 1 - $a->[2];
+        my $v2 = $b->[2] > 1 ? $b->[2] - 1 : 1 - $b->[2];
+
+        $v2 <=> $v1;
+    } @{ $data->{results} };
+
+
+    foreach my $item (@sorted[0..2]) {
+        push @{ $results{ $item->[2] > 1 ? 'long' : 'short' } }, $item;
+    }
+
+    return \%results;
+}
+
+1;
+
+package Filter::RSI;
+
+use strict;
+use warnings;
+
+use Moo;
+
+#extends Filter;
+
+sub filter {
+    my $self = shift;
+
+    my $fxapi = FXAPI->new();
+    my $data = $fxapi->screen( expression => "rsi(close,14)" );
+
+    my %results;
+
+    my @sorted = sort {
+        my $v1 = $a->[2] > 50 ? 100 - $a->[2] : $a->[2];
+        my $v2 = $b->[2] > 50 ? 100 - $b->[2] : $b->[2];
+
+        $v1 <=> $v2;
+    } @{ $data->{results} };
+
+
+    foreach my $item (@sorted[0..2]) {
+        push @{ $results{ $item->[2] > 50 ? 'long' : 'short' } }, $item;
+    }
+
+    return \%results;
+}
+
+1;
+
+package FXAPI;
+
+use strict;
+use warnings;
+
+use Moo;
+
+use LWP::Simple;
+use JSON;
+use URI::Query;
+
+
+sub screen {
+    my $self = shift;
+
+    my %args = @_;
+    my $qq = URI::Query->new(\%args);
+
+    my $url = "/screener?" . $qq->stringify;
+    return $self->fetch_url($url);
+
+}
+
+sub fetch_url {
+    my $self = shift;
+
+    my $url = "http://api.fxhistoricaldata.com" . shift;
+    my $content = get($url);
+    die($!) unless($content);
+    return from_json($content);
+}
+
+1;
---
+++ bin/fxstats.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use JSON;
+use LWP::Simple;
+use Data::Dumper;
+use Text::ASCIITable;
+
+    my %signals = (
+        short   => '/screener?expression=(close-low)/previous(atr(14),1),close,previous(atr(14),1),low',
+        long    => '/screener?expression=(high-close)/previous(atr(14),1),close,previous(atr(14),1),high',
+    );
+
+    my @work;
+    foreach my $key (keys(%signals)) {
+        my $d = fetch_url($signals{$key});
+        my @result_of_interest = grep { $_->[2] > 1 } @{ $d->{results} };
+
+        foreach my $task (@result_of_interest) {
+            push @work, { instrument => $task->[0], direction => $key, range => $task->[2], close => $task->[3], atr14 => $task->[4], pivot => $task->[5] };
+        }
+    }
+
+    my $instruments = join ",", map { $_->{instrument} } @work;
+    print "No instruments to analyse\n" and exit unless($instruments);
+    my $symbol;
+    my $url = "/descriptivestatistics?percentiles=85,90,95&expression=tr()/previous(atr(14),1)&instruments=$instruments";
+    my $data = fetch_url($url);
+
+    my $table = Text::ASCIITable->new( { headingText => 'Range stats' } );
+    $table->setCols('Instrument', 'Direction', 'Pivot', 'ATR14', 'TR/ATR14', 'Close', '85%', '90%', '95%');
+
+    foreach my $task (sort { $b->{range} <=> $a->{range} } @work) {
+        my $instrument          = $task->{instrument};
+        my $direction           = $task->{direction};
+        my $range_now           = $task->{range};
+        my $close               = $task->{close};
+        my $atr14               = $task->{atr14};
+        my $pivot               = $task->{pivot};
+        my $p                   = $data->{results}{$instrument}{percentiles};
+        my $range_85_percentile = $p->{85};
+        $table->addRow($instrument, $direction, $pivot, $atr14, sprintf("%.2f", $range_now), $close,
+            sprintf("%.2f",$p->{85}) . " (" . sprintf("%.4f",($pivot > $close ? $pivot-$p->{85}*$atr14:$pivot+$p->{85}*$atr14)) . ")",
+            sprintf("%.2f",$p->{90}) . " (" . sprintf("%.4f",($pivot > $close ? $pivot-$p->{90}*$atr14:$pivot+$p->{90}*$atr14)) . ")",
+            sprintf("%.2f",$p->{95}) . " (" . sprintf("%.4f",($pivot > $close ? $pivot-$p->{95}*$atr14:$pivot+$p->{95}*$atr14)) . ")");
+        print "SIGNAL $direction $instrument\n" if ($range_85_percentile < $range_now);
+    }
+    print $table;
+
+sub fetch_url {
+    my $url = "http://api.fxhistoricaldata.com" . shift;
+    my $content = get($url);
+    die($!) unless($content);
+    return from_json($content);
+}
