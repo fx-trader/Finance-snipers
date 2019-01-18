836d9a17f9df3eabd168f1287a93f6197444de52
Joao Costa <joaocosta@zonalivre.org>
2019-01-18 17:57:47 +0000

    Tweak variable names

--- bin/monitor_instrument.pl
+++ bin/monitor_instrument.pl
@@ -23,33 +23,34 @@ $oanda->datetime_format("UNIX");
 
 my $data = $oanda->getHistoricalData($instrument, $timeframe, 200);
 
-my $lastTimeStamp       = $data->{candles}[$#{ $data->{candles} }]{time};
-my $lastTimeStampBlock  = int($lastTimeStamp / $timeframe);
+my $thisTimeStamp       = $data->{candles}[$#{ $data->{candles} }]{time};
+my $lastTimeStampBlock  = int($thisTimeStamp / $timeframe);
 my @dataset             = map { $_->{mid}{c} } @{ $data->{candles} };
 
 my $http_response = $oanda->streamPriceData($instrument, sub {
         my $obj = shift;
+        my $print = 0;
 
-        my $latest_price = $obj->{closeoutBid} + (($obj->{closeoutAsk} - $obj->{closeoutBid}) / 2);
-        my $timestamp = $obj->{time};
-        my $thisTimeStampBlock = int($timestamp / $timeframe);
+        my $thisPrice = $obj->{closeoutBid} + (($obj->{closeoutAsk} - $obj->{closeoutBid}) / 2);
+        my $thisTimestamp = $obj->{time};
+        my $thisTimeStampBlock = int($thisTimestamp / $timeframe);
 
         if ($lastTimeStampBlock == $thisTimeStampBlock) {
-            $dataset[$#dataset] = $latest_price;
+            $dataset[$#dataset] = $thisPrice;
         } else {
+            $print = 1;
             shift @dataset;
-            push @dataset, $latest_price;
+            push @dataset, $thisPrice;
             $lastTimeStampBlock = $thisTimeStampBlock;
         }
 
-        my $datetime = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $timestamp));
+        my $datetime = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimestamp));
+        my $datetime_this_block = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimeStampBlock*$timeframe));
         my @ret = TA_RSI(0, $#dataset, \@dataset, 14);
         my $rsi = $ret[2][$#{$ret[2]}];
-        print "$datetime\t$latest_price\t", sprintf("%.2f",$rsi), "\n";
 
-        if (  $rsi < 30 ) {
-            print "RSI < 30\n";
-        } elsif ( $rsi > 70 ) {
-            print "RSI > 70\n";
-        }
+        $print = ( $print ||  $rsi < 28 || $rsi > 72 );
+
+        #print "$datetime_this_block\t$datetime\t$thisPrice\t$rsi\n";
+        print "$datetime\t$thisPrice\t", sprintf("%.2f",$rsi), "\n" if ($print);
 });
