7a541a0498160f72ce2ee4c79ffaca82681fba9c
João Costa <joaocosta@zonalivre.org>
2019-01-18 20:23:16 +0000

    tests

--- bin/bot.pl
+++ bin/bot.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # ABSTRACT: Demo oanda v20 api
-# PODNAME: monitor_instrument.pl
+# PODNAME: bot.pl
 
 use strict;
 use warnings;
@@ -13,6 +13,7 @@ use Finance::TA;
 use DateTime;
 use DateTime::Format::RFC3339;
 use Data::Dumper;
+
 
 my $timeframe   = 900;
 
@@ -27,7 +28,7 @@ my $max_requests_per_second = 2;
 my @usd_denominated = grep /(^USD_|_USD$)/, $oanda->getInstruments();
 
 MCE::Loop::init {
-    chunk_size => 6, max_workers => scalar(@usd_denominated),
+    chunk_size => 5, max_workers => scalar(@usd_denominated),
 };
 
 my %status = mce_loop {
@@ -48,36 +49,43 @@ my %status = mce_loop {
         $instruments->{$instrument_name}{dataset}             = [ map { $_->{mid}{c} } @{ $data->{candles} } ];
     }
 
-    my $http_response = $oanda->streamPriceData($chunk_ref, sub {
-        my $obj = shift;
-        my $print = 0;
+    while (1) {
 
-        my $instrument_name = $obj->{instrument};
+        print "[$instrument_names][$$][$chunk_id] - START STREAM\n";
 
-        my $thisPrice = $obj->{closeoutBid} + (($obj->{closeoutAsk} - $obj->{closeoutBid}) / 2);
-        my $thisTimestamp = $obj->{time};
-        my $thisTimeStampBlock = int($thisTimestamp / $timeframe);
+        my $http_response = $oanda->streamPriceData($chunk_ref, sub {
+            my $obj = shift;
+            my $print = 0;
 
-        if ($instruments->{$instrument_name}{lastTimeStampBlock} == $thisTimeStampBlock) {
-            $instruments->{$instrument_name}{dataset}[ $#{ $instruments->{$instrument_name}{dataset}} ] = $thisPrice
-        } else {
-            $print = 1;
-            shift @{ $instruments->{$instrument_name}{dataset} };
-            push @{ $instruments->{$instrument_name}{dataset} }, $thisPrice;
-            $instruments->{$instrument_name}{lastTimeStampBlock} = $thisTimeStampBlock;
-        }
+            my $instrument_name = $obj->{instrument};
 
-        my $datetime = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimestamp));
-        my $datetime_this_block = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimeStampBlock*$timeframe));
-        my @ret = TA_RSI(0, $#{ $instruments->{$instrument_name}{dataset} }, $instruments->{$instrument_name}{dataset}, 14);
-        my $rsi = $ret[2][$#{$ret[2]}];
+            my $thisPrice = $obj->{closeoutBid} + (($obj->{closeoutAsk} - $obj->{closeoutBid}) / 2);
+            my $thisTimestamp = $obj->{time};
+            my $thisTimeStampBlock = int($thisTimestamp / $timeframe);
 
-        $print = ( $print ||  $rsi < 28 || $rsi > 72 );
+            if ($instruments->{$instrument_name}{lastTimeStampBlock} == $thisTimeStampBlock) {
+                $instruments->{$instrument_name}{dataset}[ $#{ $instruments->{$instrument_name}{dataset}} ] = $thisPrice
+            } else {
+                $print = 1;
+                shift @{ $instruments->{$instrument_name}{dataset} };
+                push @{ $instruments->{$instrument_name}{dataset} }, $thisPrice;
+                $instruments->{$instrument_name}{lastTimeStampBlock} = $thisTimeStampBlock;
+            }
 
-        print "[$instrument_name][$$][$chunk_id] $datetime\t$thisPrice\t", sprintf("%.2f",$rsi), "\n" if ($print);
+            my $datetime = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimestamp));
+            my $datetime_this_block = $datetime_formatter->format_datetime(DateTime->from_epoch(epoch => $thisTimeStampBlock*$timeframe));
+            my @ret = TA_RSI(0, $#{ $instruments->{$instrument_name}{dataset} }, $instruments->{$instrument_name}{dataset}, 14);
+            my $rsi = $ret[2][$#{$ret[2]}];
 
-    });
+            $print = ( $print ||  $rsi < 28 || $rsi > 72 );
 
-    print "[$$][$chunk_id] EXIT\t" . $http_response->status_line . "\t" . $http_response->decoded_content . "\n";
+            print "[$instrument_name][$$][$chunk_id] $datetime\t$thisPrice\t", sprintf("%.2f",$rsi), "\n" if ($print);
+
+        });
+
+        print "[$instrument_names][$$][$chunk_id] EXIT STREAM\t" . $http_response->status_line . "\t" . $http_response->decoded_content . "\n";
+        sleep(3);
+
+    }
 
 } @usd_denominated;
