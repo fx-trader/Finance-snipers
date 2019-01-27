c22cd789935ab98e19c6d76eca5424f495d2db5f
João Costa <joaocosta@zonalivre.org>
2019-01-27 13:36:27 +0000

    run on oanda

--- reload_snipers.sh
+++ reload_snipers.sh
@@ -2,14 +2,12 @@
 
 set -euo pipefail
 
-. ~/fxcm.real
-
 SNIPER_DIR=/root/src/Finance-snipers
 cd $SNIPER_DIR
 #git pull origin master
 
 function load_sniper {
-    SNIPER_ID=sniper-${SYMBOL}-${DIRECTION}
+    SNIPER_ID=sniper-${INSTRUMENT}-${DIRECTION}
     SNIPER_CONTAINER_ID=$(docker ps -a -q -f name=${SNIPER_ID})
     if [ "$SNIPER_CONTAINER_ID" != "" ]; then
         docker rm -f ${SNIPER_ID}
@@ -20,15 +18,23 @@ function load_sniper {
             -h ${SNIPER_ID}.fxhistoricaldata.com \
             -v /root/fx/cfg:/etc/fxtrader \
             -v $SNIPER_DIR/bin:/root/snipers \
-            -e FXCM_USERNAME -e FXCM_PASSWORD -e FXCM_ACCOUNT_TYPE \
-            -e SYMBOL -e FXCM_SYMBOL \
+            -e INSTRUMENT \
             -e MAX_EXPOSURE -e EXPOSURE_INCREMENT -e DIRECTION \
             --log-driver=json-file \
             -d fxtrader/finance-hostedtrader sh -c 'exec /usr/bin/perl /root/snipers/fx-sniper.pl'
 }
 
-SYMBOL=USDOLLAR FXCM_SYMBOL='USDOLLAR' \
-MAX_EXPOSURE=5 EXPOSURE_INCREMENT=1 DIRECTION=long \
+INSTRUMENT=EUR_GBP \
+MAX_EXPOSURE=50000 \
+EXPOSURE_INCREMENT=5000 DIRECTION=short \
 load_sniper
 
-#FXCM_USERNAME=GBD118836001 FXCM_PASSWORD='5358' FXCM_ACCOUNT_TYPE=Demo \
+INSTRUMENT=XAG_USD \
+MAX_EXPOSURE=1000 \
+EXPOSURE_INCREMENT=100 DIRECTION=long \
+load_sniper
+
+INSTRUMENT=USD_CHF \
+MAX_EXPOSURE=500000 \
+EXPOSURE_INCREMENT=25000 DIRECTION=long \
+load_sniper
