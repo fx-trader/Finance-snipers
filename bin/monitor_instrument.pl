e330f4ea9189cc58c8b2d4ba8e8bf2043b5eb4be
Joao Costa <joaocosta@zonalivre.org>
2019-05-28 14:30:41 +0100

    print on demand and other tweaks

--- bin/monitor_instrument.pl
+++ bin/monitor_instrument.pl
@@ -28,10 +28,13 @@ my $thisTimeStamp       = $data->{candles}[$#{ $data->{candles} }]{time};
 my $lastTimeStampBlock  = int($thisTimeStamp / $timeframe);
 my @dataset             = map { $_->{mid}{c} } @{ $data->{candles} };
 
-my $first = 1;
+my $print = 1;
 my $http_response = $oanda->streamPriceData([$instrument], sub {
+        use Term::ReadKey;
+        ReadMode 4;
+        my $key = ReadKey(-1);
+        ReadMode 0;
         my $obj = shift;
-        my $print = 0;
 
         my $thisPrice = $obj->{closeoutBid} + (($obj->{closeoutAsk} - $obj->{closeoutBid}) / 2);
         my $thisTimestamp = $obj->{time};
@@ -51,12 +54,22 @@ my $http_response = $oanda->streamPriceData([$instrument], sub {
         my @ret = TA_RSI(0, $#dataset, \@dataset, 14);
         my $rsi = $ret[2][$#{$ret[2]}];
 
-        $print = ( $first || $print ||  $rsi < 28 || $rsi > 72 );
-        $first = 0;
+        $print = (defined($key) || $print ||  $rsi < 28 || $rsi > 72 );
 
         #print "$datetime_this_block\t$datetime\t$thisPrice\t$rsi\n";
         if ($print) {
-            my $dotIndex = index($thisPrice, ".") + 5;
-            print "$datetime\t",BOLD, RED, substr($thisPrice, 0, $dotIndex), GREY11, substr($thisPrice, $dotIndex), RESET,"\t", sprintf("%.2f",$rsi), "\n";
+
+            my $dotIndex;
+
+            while(1) {
+                $dotIndex = index($thisPrice, ".");
+                last if ($dotIndex > -1);
+                $thisPrice .= ".0";
+            }
+
+            my $greyIndex = $dotIndex + 5;
+            $thisPrice .= "0"x($greyIndex - length($thisPrice)) if ($greyIndex > length($thisPrice));
+            print "$datetime\t",BOLD, RED, substr($thisPrice, 0, $greyIndex), GREY11, substr($thisPrice, $greyIndex), RESET,"\t", sprintf("%.2f",$rsi), "\n";
         }
+        $print = 0 if ($rsi > 28 && $rsi < 72);
 });
