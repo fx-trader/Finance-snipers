e6b644d96f351d1726dd655362c3d9ba56bd35bb
João Costa <joaocosta@zonalivre.org>
2019-07-26 01:35:08 +0000

    Tweaks

--- bin/signal_check.pl
+++ bin/signal_check.pl
@@ -48,11 +48,12 @@ memoize("get_screen_result", SCALAR_CACHE => [ HASH => \%screen_result_cache ] )
 while (1) {
 
     #my $all_instruments = join(",", @{ get_all_instruments() });
-    my $all_instruments = "AUDUSD,AUDJPY,AUDNZD,CHFJPY,EURCAD,EURCHF,EURGBP,EURJPY,EURUSD,GBPCHF,GBPJPY,GBPNZD,GBPUSD,NZDUSD,NZDJPY,USDCAD,USDCHF,USDHKD,USDJPY,XAUUSD,XAGUSD,AUS200,ESP35,FRA40,GER30,HKG33,JPN225,NAS100,SPX500,UK100,UKOil,US30,USOil,USDOLLAR,Bund";
+    my $all_instruments = "AU200_AUD,AUD_CAD,AUD_CHF,AUD_HKD,AUD_JPY,AUD_NZD,AUD_SGD,AUD_USD,BCO_USD,CAD_CHF,CAD_HKD,CAD_JPY,CAD_SGD,CHF_HKD,CHF_JPY,CHF_ZAR,CN50_USD,CORN_USD,DE10YB_EUR,DE30_EUR,EU50_EUR,EUR_AUD,EUR_CAD,EUR_CHF,EUR_CZK,EUR_DKK,EUR_GBP,EUR_HKD,EUR_HUF,EUR_JPY,EUR_NOK,EUR_NZD,EUR_PLN,EUR_SEK,EUR_SGD,EUR_TRY,EUR_USD,EUR_ZAR,FR40_EUR,GBP_AUD,GBP_CAD,GBP_CHF,GBP_HKD,GBP_JPY,GBP_NZD,GBP_PLN,GBP_SGD,GBP_USD,GBP_ZAR,HK33_HKD,HKD_JPY,IN50_USD,JP225_JPY,NAS100_USD,NATGAS_USD,NL25_EUR,NZD_CAD,NZD_CHF,NZD_HKD,NZD_JPY,NZD_SGD,NZD_USD,SG30_SGD,SGD_CHF,SGD_HKD,SGD_JPY,SOYBN_USD,SPX500_USD,SUGAR_USD,TRY_JPY,TWIX_USD,UK100_GBP,UK10YB_GBP,US2000_USD,US30_USD,USB02Y_USD,USB05Y_USD,USB10Y_USD,USB30Y_USD,USD_CAD,USD_CHF,USD_CNH,USD_CZK,USD_DKK,USD_HKD,USD_HUF,USD_INR,USD_JPY,USD_MXN,USD_NOK,USD_PLN,USD_SAR,USD_SEK,USD_SGD,USD_THB,USD_TRY,USD_ZAR,WHEAT_USD,WTICO_USD,XAG_AUD,XAG_CAD,XAG_CHF,XAG_EUR,XAG_GBP,XAG_HKD,XAG_JPY,XAG_NZD,XAG_SGD,XAG_USD,XAU_AUD,XAU_CAD,XAU_CHF,XAU_EUR,XAU_GBP,XAU_HKD,XAU_JPY,XAU_NZD,XAU_SGD,XAU_USD,XAU_XAG,XCU_USD,XPD_USD,XPT_USD,ZAR_JPY,NATGAS_SUGAR,USD_EUR,DE30_USD,CHF_EUR,GBP_EUR,JP225_USD,JPY_EUR";
 
     my ($long, $short) = get_screen_result();
     my ($long_instrument, $long_rsi_date, $long_rsi) = @$long;
     my ($short_instrument, $short_rsi_date, $short_rsi) = @$short;
+    $long_instrument .= ",XCU_USD" unless $long_instrument eq 'XCU_USD';
 
     my @signals = (
 #        {   name => "LONG NOW",
@@ -344,7 +345,7 @@ sub _wants_signal_check {
 
     my $timeSinceLastError = time() - $lastSignalError;
     if ( $timeSinceLastError < 60 ) {
-        $logger->debug("signal_name: Error occurred $timeSinceLastError seconds ago, waiting for at least 60 seconds");
+        $logger->debug("$signal_name: Error occurred $timeSinceLastError seconds ago, waiting for at least 60 seconds");
         return 0;
     }
 
@@ -443,7 +444,7 @@ sub get_endpoint_result {
     $logger->trace("Fetching $url");
 
     my $response = $ua->get( $url );
-    $logger->logdie($!) unless($response->is_success);
+    $logger->logconfess($!) unless($response->is_success);
 
     my $content = $response->content;
     my $d = decode_json($content);
@@ -460,7 +461,7 @@ sub zap {
     my $email = Email::Simple->create(
         header => [
             From => 'FX Robot <robot@fxhistoricaldata.com>',
-            To => 'joaocosta@zonalivre.org',
+            To => 'fxalerts@zonalivre.org',
             Subject => $obj->{subject},
         ],
         body => $obj->{message}
